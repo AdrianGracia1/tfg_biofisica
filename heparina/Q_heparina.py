ffc7d526172a79b79b8bb2a7064e9affbae31224
Adrián Gracia García <agraciagar1@gmail.com>
2023-08-17 09:10:36 +0200

    all

---
+++ heparina/Q_heparina.py
@@ -0,0 +1,29 @@
+# Este programa calcula la carga total de una molécula dado su archivo .txt 
+# lo hace sumando las cargas de cada átomo que la forma.
+# El archivo .txt es idéntico al .pqr obtenido mediante PDB2PQR
+# excepto porque para obtener el .txt se han eliminado las dos últimas líneas del .pqr (TER y END)
+# (para que funcione correctamente lin.split())
+
+#Abriendo el fichero txt en modo lectura
+Datos_pqr = open("heparina.txt","r")
+
+#Definiendo las lineas en el txt
+Lins_pqr = Datos_pqr.readlines()
+
+#Creando una lista vacía para añadir el valor de la carga de cada átomo (línea)
+pqr_carga = []
+
+#Separando en cada linea del txt el valor de la carga y añadiendolo a la lista vacía
+for lin in Lins_pqr: 
+    a = lin.split() 
+    pqr_carga.append(float(a[8]))
+     
+#Comprobando que las lista con las cargas es correcta
+#print(pqr_carga)
+
+#Sumando los elementos de la lista de cargas para obtener la carga total de la molécula
+suma = 0.0
+for i in pqr_carga:
+	suma = suma + i
+
+print("La carga total de la molécula es: ", suma, "e", ", donde la carga del electrón es -1e")
---
+++ heparina/Readme.txt
@@ -0,0 +1,6 @@
+En este archivo se explica el contenido de esta carpeta, relativo a la Heparina.
+
+"heparina.txt" es igual que "heparina.pqr" a excepción de que faltan las dos últimas lineas (TER y END). Se utiliza para calcular la carga total de la "heparina.pqr" utilizando el programa de python "Q_heparina.py". La carga total de la molécula es:  -15.71000000000002 e , donde la carga del electrón es -1e.
+
+
+
